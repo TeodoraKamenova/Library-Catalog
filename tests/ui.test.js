a1bf5a131588282080b4b756d933e232fab1da1f
Teodora Kamenova <t.tsoneva@gmail.com>
2024-06-22 15:39:51 +0300

    Add UI tests for login page

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -81,3 +81,59 @@ test('Verify that user\'s email address is visible after user login', async ({ p
     const isUsersEmailAddressVisible = await usersEmailAddress.isVisible();
     expect(isUsersEmailAddressVisible).toBe(true);
 });
+
+// Login Page
+
+test('Login with valid creadentials', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    await page.$('a[href="/catalog"]');
+    expect(page.url()).toBe('http://localhost:3000/catalog');
+});
+
+test('Submit the form with empty input fields', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.click('input[type="submit"]');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    });
+
+    await page.$('a[href="/login"]');
+    expect(page.url()).toBe('http://localhost:3000/login');
+});
+
+test('Submit the form with empty email input field', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    });
+
+    await page.$('a[href="/login"]');
+    expect(page.url()).toBe('http://localhost:3000/login');
+});
+
+test('Submit the form with empty password input field', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.click('input[type="submit"]');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    });
+
+    await page.$('a[href="/login"]');
+    expect(page.url()).toBe('http://localhost:3000/login');
+});
