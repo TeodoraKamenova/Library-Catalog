efe132c1e8eead3b07fff0d752b43edebaf12995
Teodora Kamenova <t.tsoneva@gmail.com>
2024-06-22 15:20:51 +0300

    Add UI tests for logged-in users

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -7,8 +7,10 @@ npx playwright test tests/ui.test.js
 
 const { test, expect } = require('@playwright/test');
 
+// Navigation Bar for Guest Users
+
 test('Verify "All books" link is visible', async ({ page }) => {
-    await page.goto('http://localhost:5500');
+    await page.goto('http://localhost:3000');
     await page.waitForSelector('nav.navbar');
 
     const allBooksLink = await page.$('a[href="/catalog"]');
@@ -17,10 +19,65 @@ test('Verify "All books" link is visible', async ({ page }) => {
 });
 
 test('Verify "Login" button is visible', async ({ page }) => {
-    await page.goto('http://localhost:5500');
+    await page.goto('http://localhost:3000');
     await page.waitForSelector('nav.navbar');
 
     const loginButton = await page.$('a[href="/login"]');
     const isLoginButtonVisible = await loginButton.isVisible();
     expect(isLoginButtonVisible).toBe(true);
+});
+
+test('Verify "Register" button is visible', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.waitForSelector('nav.navbar');
+
+    const registerButton = await page.$('a[href="/register"]');
+    const isRegisterButtonVisible = await registerButton.isVisible();
+    expect(isRegisterButtonVisible).toBe(true);
+});
+
+// Navigation Bar for Logged-In Users
+
+test('Verify "All Books" button is visible after user login', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isAllBooksLinkVisible = await allBooksLink.isVisible();
+    expect(isAllBooksLinkVisible).toBe(true);
+});
+
+test('Verify "My Books" button is visible after user login', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    const myBooksLink = await page.$('a[href="/create"]');
+    const isMyBooksLinkVisible = await myBooksLink.isVisible();
+    expect(isMyBooksLinkVisible).toBe(true);
+});
+
+test('Verify "Add Book" button is visible after user login', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    const addBookLink = await page.$('a[href="/create"]');
+    const isAddBookLinkVisible = await addBookLink.isVisible();
+    expect(isAddBookLinkVisible).toBe(true);
+});
+
+test('Verify that user\'s email address is visible after user login', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    const usersEmailAddress = await page.locator('id=user');
+    const isUsersEmailAddressVisible = await usersEmailAddress.isVisible();
+    expect(isUsersEmailAddressVisible).toBe(true);
 });
